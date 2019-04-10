258747ee60e1190e3d14deffb66d73770e1b19e8
purva <purva.red@gmail.com>
2019-04-09 21:31:05 -0400

    application config

---
+++ src/ServiceLayer/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package ServiceLayer;
+
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@javax.ws.rs.ApplicationPath("resources")
+public class ApplicationConfig extends Application {
+    @Override
+    public Set<Class<?>> getClasses(){
+        return getRestResourceClasses();
+    }
+
+    private Set<Class<?>>getRestResourceClasses(){
+        Set<Class<?>> resources =
+                new java.util.HashSet<Class<?>>();
+
+        resources.add(ServiceLayer.CompanyServices.class);
+        return resources;
+    }
+}
---
+++ src/ServiceLayer/CompanyServices.java
@@ -0,0 +1,8 @@
+package ServiceLayer;
+// JAX-RS: Java API for REST Service
+import javax.ws.rs.core.*;
+import javax.ws.rs.*;
+
+public class CompanyServices {
+
+}
