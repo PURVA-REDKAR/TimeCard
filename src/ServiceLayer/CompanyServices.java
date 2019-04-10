9855f2100dcfd6ab3c8145299dea6d10c168ba47
purva <purva.red@gmail.com>
2019-04-10 12:04:30 -0400

    initial comit

--- src/ServiceLayer/CompanyServices.java
+++ src/ServiceLayer/CompanyServices.java
@@ -1,8 +1,24 @@
 package ServiceLayer;
 // JAX-RS: Java API for REST Service
-import javax.ws.rs.core.*;
+
+import companydata.*;
+
 import javax.ws.rs.*;
 
+@Path("CompanyServices")
 public class CompanyServices {
+
+    @Path("delete")
+    @GET
+    @Produces("application/json")
+    @Consumes("text/plain")
+    public String deleteCompany(
+             @QueryParam("company") String company
+    ){
+        Department dept = new Department(company,3044);
+        dept.getDeptNo();
+
+        return "{\"Area\":\"" + company + "\"}";
+    }
 
 }
